2688cb1f9e39f461d2f0e007b44278503d45d69a
dvdeer <dvdeer>
2019-08-19 17:55:57 +0100

    Broke homepage into small components, styled the home page

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Directory from '../../components/Directory/Directory';
+
+import './HomePage.scss';
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <Directory />
+    </div>
+  )
+};
+
+export default HomePage;
